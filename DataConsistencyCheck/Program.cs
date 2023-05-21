0ac974c31fa45f031b2168d6b22fa17d673fd206
Thiago Dias <thiago.dias@flipdish.com>
2023-05-21 11:17:32 +0100

    Add tests

---
+++ DataConsistencyCheck/Program.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Net.Http.Json;
+using Polly;
+
+var numberSimultOfCalls = 4;
+var numberOfExperiments = 5;
+var memcachedUrl = new Uri("https://vmw32ac5fb.execute-api.us-east-1.amazonaws.com");
+var redisUrl = new Uri("https://k89yrd3snj.execute-api.us-east-1.amazonaws.com");
+
+Console.WriteLine("Crossover Charging API Test");
+
+var retryPolicy = Policy.Handle<HttpRequestException>().Retry(3);
+
+HttpClient client = new();
+StringContent requestBody = new("{\"serviceType\": \"voice\", \"unit\": 2}");
+
+client.Dispose();
+client = new();
+
+// run memcached
+var warmupMemcached = await client.PostAsync(new Uri(memcachedUrl, "prod/charge-request-memcached"), requestBody);
+var resetMemcached = await client.PostAsync(new Uri(memcachedUrl, "prod/reset-memcached"), null);
+
+var timer = Stopwatch.StartNew();
+
+for (int j = 0; j < numberOfExperiments; j++)
+{
+
+    var errorCountMemcached = 0;
+    var balancesMemcached = new ConcurrentBag<int>();
+
+
+    var memcachedCalls = new List<Task<HttpResponseMessage>>(numberSimultOfCalls);
+    for (int i = 0; i < numberSimultOfCalls; i++)
+    {
+        var taskMemcached = retryPolicy.Execute(() => client
+            .PostAsync(new Uri(memcachedUrl, "prod/charge-request-memcached"), requestBody));
+        memcachedCalls.Add(taskMemcached);
+    }
+
+    while (memcachedCalls.Count > 0)
+    {
+        var task = await Task.WhenAny(memcachedCalls);
+        memcachedCalls.Remove(task);
+        if (!task.Result.IsSuccessStatusCode) errorCountMemcached++;
+
+        var response = await task.Result.Content.ReadFromJsonAsync<ApiResponse>();
+        balancesMemcached.Add(response.RemainingBalance);
+    }
+
+    if (errorCountMemcached > 0)
+    {
+        Console.WriteLine($"There was/were {errorCountMemcached} error(s) running Memcached requests.");
+    }
+
+    if (balancesMemcached.Distinct().Count() != balancesMemcached.Count)
+    {
+        Console.WriteLine($"There was an error on Memcached result. Balances:");
+    }
+    Console.WriteLine(string.Join(", ", balancesMemcached.OrderByDescending(r => r)));
+}
+
+timer.Stop();
+
+Console.WriteLine($"Memcached took {timer.ElapsedMilliseconds}ms.");
+
+client.Dispose();
+client = new();
+
+// run Redis
+var warmupRedis = await client.PostAsync(new Uri(redisUrl, "prod/charge-request-redis"), requestBody);
+var resetRedis = await client.PostAsync(new Uri(redisUrl, "prod/reset-redis"), null);
+
+timer = Stopwatch.StartNew();
+
+for (int j = 0; j < numberOfExperiments; j++)
+{
+
+    var errorCountRedis = 0;
+    var balancesRedis = new ConcurrentBag<int>();
+
+
+    var redisCalls = new List<Task<HttpResponseMessage>>(numberSimultOfCalls);
+    for (int i = 0; i < numberSimultOfCalls; i++)
+    {
+        var taskRedis = retryPolicy.Execute(() => client
+            .PostAsync(new Uri(redisUrl, "prod/charge-request-redis"), requestBody));
+        redisCalls.Add(taskRedis);
+    }
+
+    while (redisCalls.Count > 0)
+    {
+        var task = await Task.WhenAny(redisCalls);
+        redisCalls.Remove(task);
+        if (!task.Result.IsSuccessStatusCode) errorCountRedis++;
+
+        var response = await task.Result.Content.ReadFromJsonAsync<ApiResponse>();
+        balancesRedis.Add(response.RemainingBalance);
+    }
+
+    if (errorCountRedis > 0)
+    {
+        Console.WriteLine($"There was/were {errorCountRedis} error(s) running Redis requests.");
+    }
+
+    if (balancesRedis.Distinct().Count() != balancesRedis.Count)
+    {
+        Console.WriteLine($"There was an error on Redis result. Balances:");
+    }
+    Console.WriteLine(string.Join(", ", balancesRedis.OrderByDescending(r => r)));
+}
+
+timer.Stop();
+
+Console.WriteLine($"Redis took {timer.ElapsedMilliseconds}ms.");
+
+client.Dispose();
+
+internal class ApiResponse
+{
+    public int RemainingBalance { get; set; }
+
+    public int Charges { get; set; }
+
+    public bool IsAuthorized { get; set; }
+}
---
+++ PerformanceTesting/Program.cs
@@ -0,0 +1,118 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace CrossoverChargingTesting
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Crossover Charging API Test");
+            var summary = BenchmarkRunner.Run<MemcachedVsRedis>();
+        }
+    }
+
+    [SimpleJob(launchCount: 1, warmupCount: 1, iterationCount: 20)]
+    public class MemcachedVsRedis
+    {
+        private const string memcachedUrl = "https://vmw32ac5fb.execute-api.us-east-1.amazonaws.com/prod";
+        private const string redisUrl = "https://k89yrd3snj.execute-api.us-east-1.amazonaws.com/prod";
+
+        private readonly Uri memcachedChargeUrl = new(new Uri(memcachedUrl), "charge-request-memcached");
+        private readonly Uri memcachedResetUrl = new(new Uri(memcachedUrl), "reset-memcached");
+        private readonly Uri redisChargeUrl = new(new Uri(redisUrl), "charge-request-redis");
+        private readonly Uri redisResetUrl = new(new Uri(redisUrl), "reset-redis");
+
+        private readonly HttpClient client = new();
+        private readonly StringContent requestBody = new("{\"serviceType\": \"voice\", \"unit\": 2}");
+
+        public MemcachedVsRedis()
+        {
+            client.PostAsync(memcachedResetUrl, null);
+            client.PostAsync(redisResetUrl, null);
+        }
+
+        [Benchmark]
+        public Task<HttpResponseMessage> Memcached() => client.PostAsync(memcachedChargeUrl, requestBody);
+
+        [Benchmark]
+        public Task<HttpResponseMessage> Redis() => client.PostAsync(redisChargeUrl, requestBody);
+    }
+}
+
+//StringContent requestBody = new StringContent("{\"serviceType\": \"voice\", \"unit\": 2}");
+
+//do
+//{
+//    HttpClient client = createHttpClient();
+//    var numberOfCalls = getNumberOfCalls();
+//    //var interval = getCallsInterval();
+
+//    Stopwatch timer = Stopwatch.StartNew();
+//    List<Task<HttpResponseMessage>> calls = Enumerable.Repeat(client.PostAsync("", requestBody), numberOfCalls).ToList();
+
+//    await Task.WhenAll(calls);
+
+//    timer.Stop();
+
+//    Console.WriteLine($"Took {timer.ElapsedMilliseconds}ms.");
+
+//    client.Dispose();
+//} while (true);
+
+//HttpClient createHttpClient()
+//{
+//    Console.WriteLine("");
+//    Console.WriteLine("Select either m for Memcached or r for Redis. Or e to Exit.");
+
+//    var type = Console.ReadKey().Key;
+//    string apiUrl = "";
+
+//    switch (type)
+//    {
+//        case ConsoleKey.M:
+//            apiUrl = "https://vmw32ac5fb.execute-api.us-east-1.amazonaws.com/prod/charge-request-memcached";
+//            break;
+
+//        case ConsoleKey.R:
+//            apiUrl = "https://k89yrd3snj.execute-api.us-east-1.amazonaws.com/prod/charge-request-redis";
+//            break;
+
+//        case ConsoleKey.E:
+//            Environment.Exit(0);
+//            break;
+
+//        default: return createHttpClient();
+//    }
+
+//    return new HttpClient()
+//    {
+//        BaseAddress = new Uri(apiUrl)
+//    };
+//}
+
+//int getNumberOfCalls()
+//{
+//    Console.WriteLine("");
+//    Console.WriteLine("Number of calls");
+//    var numberOfCallsString = Console.ReadLine();
+
+//    if (int.TryParse(numberOfCallsString, out var numberOfCalls))
+//    {
+//        return numberOfCalls;
+//    }
+
+//    return getNumberOfCalls();
+//}
+//int getCallsInterval()
+//{
+//    Console.WriteLine("Calls interval in ms");
+//    var intervalString = Console.ReadLine();
+
+//    if (int.TryParse(intervalString, out var interval))
+//    {
+//        return interval;
+//    }
+
+//    return getCallsInterval();
+//}
